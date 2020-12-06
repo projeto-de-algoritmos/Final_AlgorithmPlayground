61c6c5e065481342851e0024adaabbd83620767d
Vitor Leal <vitor_leal2201@hotmail.com>
2020-12-06 15:23:37 -0300

    Add mouse events on grid
    Signed-off-by: Vitor Leal <vitor_leal2201@hotmail.com>
    
    Co-authored-by: Ezequiel <ezequiel1de1oliveira@gmail.com>

--- src/Path/index.jsx
+++ src/Path/index.jsx
@@ -6,6 +6,158 @@ export default class Path extends Component {
     this.state = {
       grid: [], 
       mouseIsPressed: false,
+      START_NODE_ROW: 5,
+      FINISH_NODE_ROW: 5,
+      START_NODE_COL: 5,
+      FINISH_NODE_COL: 15,
+      ROW_COUNT: 25,
+      COLUMN_COUNT: 35,
+      isRunning: false,
+      isStartNode: false,
+      isFinishNode: false,
+      isWallNode: false, 
+      currRow: 0,
+      currCol: 0,
+    }
+  }
+
+  componentDidMount() {
+    const grid = this.getInitialGrid();
+    this.setState({ grid });
+  }
+
+  handleMouseDown(row, col) {
+    if (!this.state.isRunning) {
+      if (this.isGridClear()) {
+        if (
+          document.getElementById(`node-${row}-${col}`).className ===
+          'node node-start'
+        ) {
+          this.setState({
+            mouseIsPressed: true,
+            isStartNode: true,
+            currRow: row,
+            currCol: col,
+          });
+        } else if (
+          document.getElementById(`node-${row}-${col}`).className ===
+          'node node-finish'
+        ) {
+          this.setState({
+            mouseIsPressed: true,
+            isFinishNode: true,
+            currRow: row,
+            currCol: col,
+          });
+        } else {
+          const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+          this.setState({
+            grid: newGrid,
+            mouseIsPressed: true,
+            isWallNode: true,
+            currRow: row,
+            currCol: col,
+          });
+        }
+      } else {
+        this.clearGrid();
+      }
+    }
+  }
+
+  isGridClear() {
+    for (const row of this.state.grid) {
+      for (const node of row) {
+        const nodeClassName = document.getElementById(
+          `node-${node.row}-${node.col}`,
+        ).className;
+        if (
+          nodeClassName === 'node node-visited' ||
+          nodeClassName === 'node node-shortest-path'
+        ) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  handleMouseEnter(row, col) {
+    if (!this.state.isRunning) {
+      if (this.state.mouseIsPressed) {
+        const nodeClassName = document.getElementById(`node-${row}-${col}`)
+          .className;
+        if (this.state.isStartNode) {
+          if (nodeClassName !== 'node node-wall') {
+            const prevStartNode = this.state.grid[this.state.currRow][
+              this.state.currCol
+            ];
+            prevStartNode.isStart = false;
+            document.getElementById(
+              `node-${this.state.currRow}-${this.state.currCol}`,
+            ).className = 'node';
+
+            this.setState({ currRow: row, currCol: col });
+            const currStartNode = this.state.grid[row][col];
+            currStartNode.isStart = true;
+            document.getElementById(`node-${row}-${col}`).className =
+              'node node-start';
+          }
+          this.setState({ START_NODE_ROW: row, START_NODE_COL: col });
+        } else if (this.state.isFinishNode) {
+          if (nodeClassName !== 'node node-wall') {
+            const prevFinishNode = this.state.grid[this.state.currRow][
+              this.state.currCol
+            ];
+            prevFinishNode.isFinish = false;
+            document.getElementById(
+              `node-${this.state.currRow}-${this.state.currCol}`,
+            ).className = 'node';
+
+            this.setState({ currRow: row, currCol: col });
+            const currFinishNode = this.state.grid[row][col];
+            currFinishNode.isFinish = true;
+            document.getElementById(`node-${row}-${col}`).className =
+              'node node-finish';
+          }
+          this.setState({ FINISH_NODE_ROW: row, FINISH_NODE_COL: col });
+        } else if (this.state.isWallNode) {
+          const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+          this.setState({ grid: newGrid });
+        }
+      }
+    }
+  }
+
+  handleMouseUp(row, col) {
+    if (!this.state.isRunning) {
+      this.setState({ mouseIsPressed: false });
+      if (this.state.isStartNode) {
+        const isStartNode = !this.state.isStartNode;
+        this.setState({ isStartNode, START_NODE_ROW: row, START_NODE_COL: col });
+      } else if (this.state.isFinishNode) {
+        const isFinishNode = !this.state.isFinishNode;
+        this.setState({
+          isFinishNode,
+          FINISH_NODE_ROW: row,
+          FINISH_NODE_COL: col,
+        });
+      }
+      this.getInitialGrid();
+    }
+  }
+
+  handleMouseLeave() {
+    if (this.state.isStartNode) {
+      const isStartNode = !this.state.isStartNode;
+      this.setState({ isStartNode, mouseIsPressed: false });
+    } else if (this.state.isFinishNode) {
+      const isFinishNode = !this.state.isFinishNode;
+      this.setState({ isFinishNode, mouseIsPressed: false });
+    } else if (this.state.isWallNode) {
+      const isWallNode = !this.state.isWallNode;
+      this.setState({ isWallNode, mouseIsPressed: false });
+      this.getInitialGrid();
     }
   }
 
