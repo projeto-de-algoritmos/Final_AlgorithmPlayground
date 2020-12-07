e48e411249efb0d17cba54740216fdeda7e0d8e3
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-12-06 23:14:56 -0300

    Using algorithms
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
    Co-authored-by: Vitor Leal <vitor_leal2201@hotmail.com>

--- src/Path/index.jsx
+++ src/Path/index.jsx
@@ -1,5 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Node from './Node';
+import { dijkstra } from '../algorithms/dijkstra';
+import { bfs } from '../algorithms/bfs';
+import { dfs } from '../algorithms/dfs';
+
 
 import './styles.css';
 
@@ -31,11 +35,11 @@ export default class Path extends Component {
 
   componentDidMount() {
     const grid = this.getInitialGrid();
-    this.setState({grid});
+    this.setState({ grid });
   }
 
   toggleIsRunning() {
-    this.setState({isRunning: !this.state.isRunning});
+    this.setState({ isRunning: !this.state.isRunning });
   }
 
   toggleView() {
@@ -49,7 +53,7 @@ export default class Path extends Component {
           this.state.ROW_COUNT,
           this.state.COLUMN_COUNT,
         );
-        this.setState({isDesktopView, grid});
+        this.setState({ isDesktopView, grid });
       } else {
         if (
           this.state.START_NODE_ROW > this.state.MOBILE_ROW_COUNT ||
@@ -63,7 +67,7 @@ export default class Path extends Component {
             this.state.MOBILE_ROW_COUNT,
             this.state.MOBILE_COLUMN_COUNT,
           );
-          this.setState({isDesktopView, grid});
+          this.setState({ isDesktopView, grid });
         }
       }
     }
@@ -175,13 +179,13 @@ export default class Path extends Component {
               `node-${this.state.currRow}-${this.state.currCol}`,
             ).className = 'node';
 
-            this.setState({currRow: row, currCol: col});
+            this.setState({ currRow: row, currCol: col });
             const currStartNode = this.state.grid[row][col];
             currStartNode.isStart = true;
             document.getElementById(`node-${row}-${col}`).className =
               'node node-start';
           }
-          this.setState({START_NODE_ROW: row, START_NODE_COL: col});
+          this.setState({ START_NODE_ROW: row, START_NODE_COL: col });
         } else if (this.state.isFinishNode) {
           if (nodeClassName !== 'node node-wall') {
             const prevFinishNode = this.state.grid[this.state.currRow][
@@ -192,16 +196,16 @@ export default class Path extends Component {
               `node-${this.state.currRow}-${this.state.currCol}`,
             ).className = 'node';
 
-            this.setState({currRow: row, currCol: col});
+            this.setState({ currRow: row, currCol: col });
             const currFinishNode = this.state.grid[row][col];
             currFinishNode.isFinish = true;
             document.getElementById(`node-${row}-${col}`).className =
               'node node-finish';
           }
-          this.setState({FINISH_NODE_ROW: row, FINISH_NODE_COL: col});
+          this.setState({ FINISH_NODE_ROW: row, FINISH_NODE_COL: col });
         } else if (this.state.isWallNode) {
           const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
-          this.setState({grid: newGrid});
+          this.setState({ grid: newGrid });
         }
       }
     }
@@ -209,10 +213,10 @@ export default class Path extends Component {
 
   handleMouseUp(row, col) {
     if (!this.state.isRunning) {
-      this.setState({mouseIsPressed: false});
+      this.setState({ mouseIsPressed: false });
       if (this.state.isStartNode) {
         const isStartNode = !this.state.isStartNode;
-        this.setState({isStartNode, START_NODE_ROW: row, START_NODE_COL: col});
+        this.setState({ isStartNode, START_NODE_ROW: row, START_NODE_COL: col });
       } else if (this.state.isFinishNode) {
         const isFinishNode = !this.state.isFinishNode;
         this.setState({
@@ -228,34 +232,140 @@ export default class Path extends Component {
   handleMouseLeave() {
     if (this.state.isStartNode) {
       const isStartNode = !this.state.isStartNode;
-      this.setState({isStartNode, mouseIsPressed: false});
+      this.setState({ isStartNode, mouseIsPressed: false });
     } else if (this.state.isFinishNode) {
       const isFinishNode = !this.state.isFinishNode;
-      this.setState({isFinishNode, mouseIsPressed: false});
+      this.setState({ isFinishNode, mouseIsPressed: false });
     } else if (this.state.isWallNode) {
       const isWallNode = !this.state.isWallNode;
-      this.setState({isWallNode, mouseIsPressed: false});
+      this.setState({ isWallNode, mouseIsPressed: false });
       this.getInitialGrid();
     }
   }
+
+
+  clearGrid() {
+    if (!this.state.isRunning) {
+      const newGrid = this.state.grid.slice();
+      for (const row of newGrid) {
+        for (const node of row) {
+          let nodeClassName = document.getElementById(
+            `node-${node.row}-${node.col}`,
+          ).className;
+          if (
+            nodeClassName !== 'node node-start' &&
+            nodeClassName !== 'node node-finish' &&
+            nodeClassName !== 'node node-wall'
+          ) {
+            document.getElementById(`node-${node.row}-${node.col}`).className =
+              'node';
+            node.isVisited = false;
+            node.distance = Infinity;
+            node.distanceToFinishNode =
+              Math.abs(this.state.FINISH_NODE_ROW - node.row) +
+              Math.abs(this.state.FINISH_NODE_COL - node.col);
+          }
+          if (nodeClassName === 'node node-finish') {
+            node.isVisited = false;
+            node.distance = Infinity;
+            node.distanceToFinishNode = 0;
+          }
+          if (nodeClassName === 'node node-start') {
+            node.isVisited = false;
+            node.distance = Infinity;
+            node.distanceToFinishNode =
+              Math.abs(this.state.FINISH_NODE_ROW - node.row) +
+              Math.abs(this.state.FINISH_NODE_COL - node.col);
+            node.isStart = true;
+            node.isWall = false;
+            node.previousNode = null;
+            node.isNode = true;
+          }
+        }
+      }
+    }
+  }
+
   visualize(algo) {
     if (!this.state.isRunning) {
       this.clearGrid();
       this.toggleIsRunning();
-      const {grid} = this.state;
+      const { grid } = this.state;
       const startNode =
         grid[this.state.START_NODE_ROW][this.state.START_NODE_COL];
       const finishNode =
         grid[this.state.FINISH_NODE_ROW][this.state.FINISH_NODE_COL];
       let visitedNodesInOrder;
+      switch (algo) {
+        case 'Dijkstra':
+          visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+          break;
+        case 'BFS':
+          visitedNodesInOrder = bfs(grid, startNode, finishNode);
+          break;
+        case 'DFS':
+          visitedNodesInOrder = dfs(grid, startNode, finishNode);
+          break;
+        default:
+          // should never get here
+          break;
+      }
       const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
       nodesInShortestPathOrder.push('end');
       this.animate(visitedNodesInOrder, nodesInShortestPathOrder);
     }
   }
 
-    render() {
-    const {grid, mouseIsPressed} = this.state;
+  animate(visitedNodesInOrder, nodesInShortestPathOrder) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          this.animateShortestPath(nodesInShortestPathOrder);
+        }, 10 * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        const nodeClassName = document.getElementById(
+          `node-${node.row}-${node.col}`,
+        ).className;
+        if (
+          nodeClassName !== 'node node-start' &&
+          nodeClassName !== 'node node-finish'
+        ) {
+          document.getElementById(`node-${node.row}-${node.col}`).className =
+            'node node-visited';
+        }
+      }, 10 * i);
+    }
+  }
+
+  animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      if (nodesInShortestPathOrder[i] === 'end') {
+        setTimeout(() => {
+          this.toggleIsRunning();
+        }, i * 50);
+      } else {
+        setTimeout(() => {
+          const node = nodesInShortestPathOrder[i];
+          const nodeClassName = document.getElementById(
+            `node-${node.row}-${node.col}`,
+          ).className;
+          if (
+            nodeClassName !== 'node node-start' &&
+            nodeClassName !== 'node node-finish'
+          ) {
+            document.getElementById(`node-${node.row}-${node.col}`).className =
+              'node node-shortest-path';
+          }
+        }, i * 40);
+      }
+    }
+  }
+
+  render() {
+    const { grid, mouseIsPressed } = this.state;
     return (
       <div>
         <table
@@ -266,7 +376,7 @@ export default class Path extends Component {
               return (
                 <tr key={rowIdx}>
                   {row.map((node, nodeIdx) => {
-                    const {row, col, isFinish, isStart, isWall} = node;
+                    const { row, col, isFinish, isStart, isWall } = node;
                     return (
                       <Node
                         key={nodeIdx}
@@ -282,7 +392,7 @@ export default class Path extends Component {
                           this.handleMouseEnter(row, col)
                         }
                         onMouseUp={() => this.handleMouseUp(row, col)}
-                        row={row}/>
+                        row={row} />
                     );
                   })}
                 </tr>
@@ -290,6 +400,24 @@ export default class Path extends Component {
             })}
           </tbody>
         </table>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => this.visualize('Dijkstra')}>
+          Dijkstra's
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => this.visualize('BFS')}>
+          Bread First Search
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => this.visualize('DFS')}>
+          Depth First Search
+        </button>
       </div>
     );
   }
